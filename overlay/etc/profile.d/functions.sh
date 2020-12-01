2d318ab444c788247fc51e2f0536f1846b731c54
Ben Green <ben@bcgdesign.com>
2020-12-01 14:54:52 +0000

    Removing functions, using underscore echo functions in installation script

--- overlay/etc/profile.d/functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-e_colour () {
-    NC='\033[0m'
-    echo -e " - ${1}${2}${NC}"
-}
-e () { e_colour '\033[0m' "${1}"; }
-e_exit () { e_colour '\033[0;31m' "${1}"; exit 1; }
-e_done () { e_colour '\033[1;32m' "done"; }
