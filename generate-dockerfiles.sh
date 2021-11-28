a2ea01baedcc980b5ace6a514259d610c9302e44
Ben Green <ben@bcgdesign.com>
2021-11-28 08:59:01 +0000

    Simplifying Dockerfile generate script

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -2,27 +2,21 @@
 
 set -euo pipefail
 
-
 BASE_REVISION="1.2.1"
 echo "Base: ${BASE_REVISION}"
 
-declare -A ALPINE_VERSIONS
-ALPINE_VERSIONS["3.12"]="3.12.9"
-ALPINE_VERSIONS["3.13"]="3.13.7"
-ALPINE_VERSIONS["3.14"]="3.14.3"
-ALPINE_VERSIONS["3.15"]="3.15.0"
-ALPINE_VERSIONS["edge"]="edge"
-
-for V in "${!ALPINE_VERSIONS[@]}" ; do
+ALPINE_VERSIONS="3.12 3.13 3.14 3.15 edge"
+for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine: ${V}"
+    ALPINE_REVISION=`cat ./${V}/ALPINE_REVISION`
 
     DOCKERFILE=$(docker run \
         -v /mnt/q/src/docker/alpine-s6/Dockerfile.esh:/Dockerfile.esh \
         bfren/alpine esh \
         "/Dockerfile.esh" \
         BASE_REVISION=${BASE_REVISION} \
-        ALPINE_REVISION=${ALPINE_VERSIONS[${V}]}
+        ALPINE_REVISION=${ALPINE_REVISION}
     )
 
     echo "${DOCKERFILE}" > ./${V}/Dockerfile
