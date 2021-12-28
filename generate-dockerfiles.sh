933b1bc26323f92c579cdfc75ec7d57ca3e2a702
bfren <ben@bcgdesign.com>
2021-12-28 09:07:07 +0000

    v3.2.2 (#64)
    Minor updates
    * Updating Dockerfiles and base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
-BASE_REVISION="1.2.2"
+docker pull bfren/alpine
+
+BASE_REVISION="1.3.2"
 echo "Base: ${BASE_REVISION}"
 
 ALPINE_VERSIONS="3.12 3.13 3.14 3.15 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
-    echo "Alpine: ${V}"
+    echo "Alpine ${V}"
     ALPINE_REVISION=`cat ./${V}/ALPINE_REVISION`
 
     DOCKERFILE=$(docker run \
