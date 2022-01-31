46e77c180b6c7164e07cbdfecf9b61a2f59f6e92
Ben Green <ben@bcgdesign.com>
2022-01-31 08:19:25 +0000

    Adding run executable for testing

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker buildx build \
+    --build-arg BF_IMAGE=alpine-s6 \
+    --build-arg BF_VERSION=4.0.0 \
+    -f 3.15/Dockerfile \
+    -t alpine-s6-dev \
+    .
+
+docker run -it alpine-s6-dev sh
