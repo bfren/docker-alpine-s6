92ad8c665b57d80a146fd177fc6d23a08d5ae94b
Ben Green <ben@bcgdesign.com>
2022-02-02 12:22:26 +0000

    Using latest base images

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+VERSION=`cat VERSION`
+
 git pull || true
 
 docker buildx build \
     --build-arg BF_IMAGE=alpine-s6 \
-    --build-arg BF_VERSION=4.0.0 \
+    --build-arg BF_VERSION=${VERSION} \
     -f 3.15/Dockerfile \
     -t alpine-s6-dev \
     .
