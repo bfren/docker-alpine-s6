5a96f95546904d1641c0b9b70d610b7bc58b364e
bfren <ben@bcgdesign.com>
2022-02-10 17:43:05 +0000

    v4.0.0 (#66)
    Major updates
    * Updating to S6 Overlay 3.0.0.2 - **many breaking changes** including shebang of `/command/with-contenv sh`
    * Service definitions now live in `/etc/s6-overlay/s6-rc.d` and have a completely new format
    * Container init files now live in `/etc/bf/init.d` but work in the same way
    * Permissions have a new format (using `bf-ch`) and now live in `/etc/bf/ch.d`
    * Moving bf executables to `/usr/bin/bf` and scripts to `/usr/lib/bf`
    
    Minor updates
    * Many more tweaks and improvements to codebase
    
    Build updates
    * `run.sh` allows quick testing
    
    Documentation updates
    * Updating copyright year to 2022

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="1.3.3"
+BASE_REVISION="1.4.8"
 echo "Base: ${BASE_REVISION}"
 
-ALPINE_VERSIONS="3.12 3.13 3.14 3.15 edge"
+ALPINE_VERSIONS="3.8 3.12 3.13 3.14 3.15 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine ${V}"
@@ -15,6 +15,7 @@ for V in ${ALPINE_VERSIONS} ; do
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
         BASE_REVISION=${BASE_REVISION} \
@@ -25,4 +26,5 @@ for V in ${ALPINE_VERSIONS} ; do
 
 done
 
+docker system prune -f
 echo "Done."
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=`cat VERSION`
+
+git pull || exit 1
+chmod +x run.sh
+
+docker buildx build \
+    --build-arg BF_IMAGE=alpine-s6 \
+    --build-arg BF_VERSION=${VERSION} \
+    -f 3.15/Dockerfile \
+    -t alpine-s6-dev \
+    .
+
+docker run -it alpine-s6-dev sh
