84954cc63ad9900d7a0cf6d332b83a6fe8aba7d1
bfren <ben@bcgdesign.com>
2022-03-04 09:58:28 +0000

    v4.1.0 (#77)
    Major updates
    * Adding Alpine 3.9, 3.10, and 3.11
    
    Minor updates
    * Using latest base images
    
    Documentation updates
    * Adding 3.8 build status shield

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="1.4.13"
+BASE_REVISION="1.5.0"
 echo "Base: ${BASE_REVISION}"
 
-ALPINE_VERSIONS="3.8 3.12 3.13 3.14 3.15 edge"
+ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine ${V}"
--- run.sh
+++ run.sh
@@ -8,8 +8,7 @@ chmod +x run.sh
 docker buildx build \
     --build-arg BF_IMAGE=alpine-s6 \
     --build-arg BF_VERSION=${VERSION} \
-    -f 3.15/Dockerfile \
+    -f ${1:-3.15}/Dockerfile \
     -t alpine-s6-dev \
-    .
-
-docker run -it alpine-s6-dev sh
+    . \
+    && docker run -it alpine-s6-dev sh
