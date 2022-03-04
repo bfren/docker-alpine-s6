66beb1a471f1146f7a27bc11710f278297b264a0
Ben Green <ben@bcgdesign.com>
2022-03-04 09:50:13 +0000

    Build and run in one command

--- run.sh
+++ run.sh
@@ -10,6 +10,5 @@ docker buildx build \
     --build-arg BF_VERSION=${VERSION} \
     -f 3.15/Dockerfile \
     -t alpine-s6-dev \
-    .
-
-docker run -it alpine-s6-dev sh
+    . \
+    && docker run -it alpine-s6-dev sh
