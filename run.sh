9660e4a435b4fbd20555dc22ec150d918e7c905d
bfren <ben@bcgdesign.com>
2022-03-07 08:50:38 +0000

    v4.1.1 (#78)
    Minor updates
    * Using latest base images
    
    Development updates
    * Git pull after checkout
    * No need to pull before run any more

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-VERSION=`cat VERSION`
-
-git pull || exit 1
-chmod +x run.sh
+IMAGE=`cat VERSION`
+ALPINE=${1:-3.15}
 
 docker buildx build \
     --build-arg BF_IMAGE=alpine-s6 \
-    --build-arg BF_VERSION=${VERSION} \
-    -f ${1:-3.15}/Dockerfile \
-    -t alpine-s6-dev \
+    --build-arg BF_VERSION=${IMAGE} \
+    -f ${ALPINE}/Dockerfile \
+    -t alpine${ALPINE}-s6-dev \
     . \
-    && docker run -it alpine-s6-dev sh
+    && \
+    docker run -it alpine${ALPINE}-s6-dev sh
