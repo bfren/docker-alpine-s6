03e2e5cdd28c15f4b0ebfdd0165f903bbd4494fa
Ben Green <ben@bcgdesign.com>
2022-03-07 08:41:09 +0000

    No need to pull before run any more

--- run.sh
+++ run.sh
@@ -3,13 +3,11 @@
 IMAGE=`cat VERSION`
 ALPINE=${1:-3.15}
 
-git pull || exit 1
-chmod +x run.sh
-
 docker buildx build \
     --build-arg BF_IMAGE=alpine-s6 \
     --build-arg BF_VERSION=${IMAGE} \
     -f ${ALPINE}/Dockerfile \
     -t alpine${ALPINE}-s6-dev \
     . \
-    && docker run -it alpine${ALPINE}-s6-dev sh
+    && \
+    docker run -it alpine${ALPINE}-s6-dev sh
