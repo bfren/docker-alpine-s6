693b1fde7623b30f9bcab0e926c6f4fed86d54e8
bfren <bf@bfren.dev>
2022-06-23 10:57:52 +0100

    Reducing timeout

--- verify.sh
+++ verify.sh
@@ -4,5 +4,5 @@ IMAGE="alpine-s6"
 TAG="${1}"
 
 echo "Running ${IMAGE}:${TAG}"
-timeout --signal=KILL 120s bash -c "docker run bfren/${IMAGE}:${TAG} bf-image"
+timeout --signal=KILL 60s bash -c "docker run bfren/${IMAGE}:${TAG} bf-image"
 exit $?
