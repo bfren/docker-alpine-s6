6801a704768db7a2fed0b2f52f7b7b44fb7a4239
bfren <bf@bfren.dev>
2022-06-23 11:19:40 +0100

    Updating verify.sh process

--- verify.sh
+++ verify.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-IMAGE="alpine-s6"
-TAG="${1}"
-
-echo "Running ${IMAGE}:${TAG}"
-timeout --signal=KILL 60s bash -c "docker run bfren/${IMAGE}:${TAG} bf-image"
-exit $?
+IMAGE="${1}"
+PLATFORMS="${2}"
+for P in `echo ${PLATFORMS} | tr "," " "` ; do
+    timeout --signal=KILL 60s bash -c "docker run --rm --platform=${P} bfren/${IMAGE}:test bf-image"
+    [[ "$?" -ne "0" ]] && exit $?
+done
