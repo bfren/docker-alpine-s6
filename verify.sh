eb483fb0230667c9e77190cc9253364ad3ad4df0
bfren <bf@bfren.dev>
2022-06-23 11:27:19 +0100

    Fixing test tag

--- verify.sh
+++ verify.sh
@@ -3,6 +3,6 @@
 IMAGE="${1}"
 PLATFORMS="${2}"
 for P in `echo ${PLATFORMS} | tr "," " "` ; do
-    timeout --signal=KILL 60s bash -c "docker run --rm --platform=${P} bfren/${IMAGE}:test bf-image"
+    timeout --signal=KILL 60s bash -c "docker run --rm --platform=${P} ${IMAGE} bf-image"
     [[ "$?" -ne "0" ]] && exit $?
 done
