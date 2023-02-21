3b0463567e9c157b54a44c59a3bd8901b70bb702
bfren <bf@bfren.dev>
2023-02-21 06:37:00 +0000

    Updating S6 Overlay 3.1.4.1

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 docker pull bfren/alpine
 
 BASE_REVISION="1.7.10"
-S6_VERSION="3.1.4.0"
+S6_VERSION="3.1.4.1"
 echo "Base: ${BASE_REVISION}"
 
 ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 3.16 3.17 edge"
