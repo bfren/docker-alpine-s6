4c0f4fd70d60644f4ae6b26224556f62627fd448
bfren <ben@bcgdesign.com>
2023-05-10 16:50:11 +0100

    v4.5.0 (#128)
    Major updates
    * Using latest base images
    * Adding Alpine 3.18
    
    Development updates
    * Use 3.18 as the default version to run

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="1.7.13"
+BASE_REVISION="1.8.0"
 S6_VERSION="3.1.5.0"
 echo "Base: ${BASE_REVISION}"
 
-ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 3.16 3.17 edge"
+ALPINE_VERSIONS="3.8 3.9 3.10 3.11 3.12 3.13 3.14 3.15 3.16 3.17 3.18 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Alpine ${V}"
