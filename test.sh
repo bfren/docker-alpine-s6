30fdf3b8c0ee06042a5a54b241a80220583a7884
bfren <bf@bfren.dev>
2024-02-23 17:44:58 +0000

    Using entrypoint override for tests

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-IMAGE=`cat VERSION`
+IMAGE=alpine-s6
+VERSION=`cat VERSION`
 ALPINE=${1:-3.19}
+TAG=${IMAGE}-test
 
 docker buildx build \
     --load \
-    --build-arg BF_IMAGE=alpine-s6 \
-    --build-arg BF_VERSION=${IMAGE} \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
     -f ${ALPINE}/Dockerfile \
-    -t alpine${ALPINE}-s6-test \
+    -t ${TAG} \
     . \
     && \
-    docker run --rm alpine${ALPINE}-s6-test env -i nu -c "use bf test ; test"
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
