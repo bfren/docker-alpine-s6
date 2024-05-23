02178c9b170a86342c40434f03b0374d289abeb2
bfren <ben@bcgdesign.com>
2024-05-23 08:13:11 -0500

    v5.3.0 (#157)
    Minor updates
    * Using latest base images
    * Adding Alpine 3.20 and making it the default

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="2.2.7"
+BASE_VERSION="2.3.0"
 S6_VERSION="3.1.6.2"
 echo "Base: ${BASE_VERSION}"
 
-ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19"
+ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19 3.20"
 for E in ${ALPINE_EDITIONS} ; do
 
     echo "Alpine ${E}"
