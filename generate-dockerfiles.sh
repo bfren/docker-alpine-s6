b20976b202a0b8089c0a803963da7e198be59150
bfren <bf@bfren.dev>
2024-12-06 13:27:09 +0000

    Adding Alpine 3.21 and making it the default Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="2.7.8"
+BASE_VERSION="2.8.0"
 S6_VERSION="3.2.0.2"
 echo "Base: ${BASE_VERSION}"
 
-ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19 3.20"
+ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19 3.20 3.21"
 for E in ${ALPINE_EDITIONS} ; do
 
     echo "Alpine ${E}"
