7df3967001b8cda8bd85b4af90ffa35d5d8f522e
bfren <bf@bfren.dev>
2025-05-09 09:16:09 +0100

    Using latest base images Updating to S6 Overlay 3.2.1.0

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="2.8.4"
-S6_VERSION="3.2.0.2"
+BASE_VERSION="2.8.5"
+S6_VERSION="3.2.1.0"
 echo "Base: ${BASE_VERSION}"
 
 ALPINE_EDITIONS="3.15 3.16 3.17 3.18 3.19 3.20 3.21"
